3bb5dedcdb0bd165fc8fc09667da0b59b65e742c
Paidi Akileswar <155959310+paidiakileswar@users.noreply.github.com>
2024-05-03 21:10:23 +0530

    Add files via upload

---
+++ requirements_cuda.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+ubuntuVersion='22.04'
+cudaEnabled=true
+asanEnabled=false
+
+sudo apt-get update && sudo apt-get install -y \
+  build-essential \
+  ninja-build \
+  libboost-program-options-dev \
+  libboost-filesystem-dev \
+  libboost-graph-dev \
+  libboost-system-dev \
+  libboost-test-dev \
+  libeigen3-dev \
+  libceres-dev \
+  libflann-dev \
+  libfreeimage-dev \
+  libmetis-dev \
+  libgoogle-glog-dev \
+  libgflags-dev \
+  libsqlite3-dev \
+  libglew-dev \
+  qtbase5-dev \
+  libqt5opengl5-dev \
+  libcgal-dev \
+  libcgal-qt5-dev \
+  libgl1-mesa-dri \
+  libunwind-dev \
+  xvfb
+
+git clone https://github.com/json87/SphereSfM.git
+cd SphereSfM
+
+sudo apt-get install -y \
+  nvidia-cuda-toolkit-gcc \
+  gcc-10 g++-10
+
+export CC=/usr/bin/gcc-10
+export CXX=/usr/bin/g++-10
+export CUDAHOSTCXX=/usr/bin/g++-10
+
+cmake --version
+mkdir build && cd build
+cmake .. \
+  -GNinja \
+  -DCMAKE_CUDA_ARCHITECTURES=50 
+ninja
+
+export DISPLAY=":99.0"
+export QT_QPA_PLATFORM="offscreen"
+Xvfb :99 &
