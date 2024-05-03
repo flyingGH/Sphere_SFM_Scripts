e64939934133ca54669d6fa49c6ea94ba764768c
Paidi Akileswar <155959310+paidiakileswar@users.noreply.github.com>
2024-05-03 23:21:01 +0530

    Update requirements_no_cuda.sh

--- requirements_no_cuda.sh
+++ requirements_no_cuda.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# Libraries needed for sphere-sfm Setup
 sudo apt-get update && sudo apt-get install -y \
   build-essential \
   ninja-build \
@@ -25,21 +26,43 @@ sudo apt-get update && sudo apt-get install -y \
   awscli \
   xvfb
 
+#Ceres must be installed before
+
+# ceres installation skip if installed
+sudo apt-get update
+sudo apt-get install -y \
+    cmake \
+    libgoogle-glog-dev \
+    libgflags-dev \
+    libatlas-base-dev \
+    libeigen3-dev \
+    libsuitesparse-dev
+
+wget http://ceres-solver.org/ceres-solver-2.2.0.tar.gz
+tar zxf ceres-solver-2.2.0.tar.gz
+mkdir ceres-bin && cd ceres-bin
+cmake ../ceres-solver-2.2.0
+make -j3
+sudo make install
+
+#Clone Repo
 git clone https://github.com/json87/SphereSfM.git
 cd SphereSfM
 
+# Install cmake
 sudo apt install -y cmake
 cmake --version
 
-mkdir build
-cd build
+mkdir build && cd build
 
+#Start build
 cmake .. -GNinja
-
 ninja
 
+#install the build
 sudo ninja install
 
+# Export to see colmap display
 export DISPLAY=":99.0"
 export QT_QPA_PLATFORM="offscreen"
 
